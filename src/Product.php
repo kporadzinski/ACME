065cf753f5915560698d08c1003f6da6172eac16
kzieba <krzysztofkamilzieba@gmail.com>
2016-07-25 15:47:30 +0200

    poprawki w klasie Product

--- src/Product.php
+++ src/Product.php
@@ -9,9 +9,9 @@ class Product
     protected $description;
     protected $category;
     protected $supply;
-    protected $dataBaseConnection;
+    public $dataBaseConnection;
 
-    public function construct($dataBaseConnection)
+    public function __construct($dataBaseConnection)
     {
         $this->id = -1;
         $this->dataBaseConnection = $dataBaseConnection;
@@ -47,7 +47,99 @@ class Product
         return $this->supply;
     }
 
-    public function setId($id)
+    public function getProductById($id = -1)
+    {
+        if($id == (-1)){
+            $query = "SELECT * FROM `products`";
+        }else{
+            $query = "SELECT * FROM `products` WHERE `id` = $id";
+        }
+        $result = $this->dataBaseConnection->query($query);
+        $booksArray = [];
+        if(!$result){
+            return NULL;
+        } else {
+            foreach($result as $row){
+                $this->id = $row['id'];
+                $this->setName($row['name']);
+                $this->setPrice($row['price']); 
+                $this->setDescription($row['description']);
+                $this->setCategory($row['category']);
+                $this->setSupply($row['supply']);
+                $booksArray[] = $this->jsonSerialize();
+            }
+            return $booksArray;
+            //return json_encode($booksArray);
+        }
+    }
+    public function jsonSerialize()
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'price' => $this->price,
+            'description' => $this->description,
+            'category' => $this->category,
+            'supply' => $this->supply
+        ];
+    }
+    
+    
+    public function getProductByCategory($category = -1)
+    {
+        if($category == (-1)){
+            $query = "SELECT * FROM `products`";
+        }else{
+            $query = "SELECT * FROM `products` WHERE `category` = $category";
+        }
+        $result = $this->dataBaseConnection->query($query);
+        $booksArray = [];
+        if(!$result){
+            return NULL;
+        } else {
+            foreach($result as $row){
+                $this->id = $row['id'];
+                $this->setName($row['name']);
+                $this->setPrice($row['price']); 
+                $this->setDescription($row['description']);
+                $this->setCategory($row['category']);
+                $this->setSupply($row['supply']);
+                $booksArray[] = $this->jsonSerialize();
+            }
+            return $booksArray;
+            //return json_encode($booksArray);
+        }
+    }
+    
+    public function getProductPhotos($id = -1)
+    {
+        if($id == (-1)){
+            $id = $this->id;
+        }
+        $query = "SELECT * FROM `photos` WHERE `product_id` = $id";
+        $result = $this->dataBaseConnection->query($query);
+        $booksArray = [];
+        if(!$result){
+            return NULL;
+        } else {
+            foreach($result as $row){
+                $booksArray[] = $row['file_path'];
+            }
+            return $booksArray;
+            //return json_encode($booksArray);
+        }
+    }
+    
+    public function addPhoto($id = -1)
+    {
+        if($id == (-1)){
+            $id = $this->id;
+        }
+        $query = "INSERT INTO `acme`.`photos` (`id` ,`product_id` ,`file_path`) VALUES (NULL , '$id', '/photo3.jpg');";
+        $result = $this->dataBaseConnection->query($query);
+    }
+    
+    public function setId($id)//probably not to be used at all 
     {
         $this->id = $id;
     }
